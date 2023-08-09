015a9c8134a2c06e3e0ad4413f6c18221c9bba74
Theodoros Anagnostopoulos <theodoros.anagnostopoulos@ibm.com>
2023-08-09 11:38:17 +0300

    fixed date added search all

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateUserAPIView, GetAllUser, SearchUserByName, SearchUserByAge
+from .views import CreateUserAPIView, GetAllUser, SearchUserByName, SearchUserByAge, SearchCustom
 
 urlpatterns = [
     path('create/', CreateUserAPIView.as_view(), name="create"),
     path('allusers/', GetAllUser.as_view(), name="all users"),
     path('filter/', SearchUserByName.as_view(), name="filter"),
     path('filterb/', SearchUserByAge.as_view(), name="filter"),
+    path('filterall/', SearchCustom.as_view(), name="filter"),
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
+from datetime import date
 
 # Create your views here.
 User = get_user_model()
@@ -41,10 +42,32 @@ class SearchUserByAge(APIView):
     permission_classes = (AllowAny,)
     def get(self,request):
         today = timezone.now().date()
-        age_lower = today 
-        age_lower.year = today.year - request.data['ageL']
-        age_upper = today
-        age_upper.year = today.year - request.data['ageU']
-        user = User.objects.all().filter(birthday__gt = age_lower, birthday__lt = age_upper)
+        age_lower = date(year=today.year - request.data['ageL'], month=today.month, day=today.day)
+        age_upper = date(year=today.year - request.data['ageU'], month=today.month, day=today.day)
+        user = User.objects.all().filter(birthday__lt = age_lower, birthday__gt = age_upper)
         user_serializer = UserSerializer(user, many=True)
         return Response(user_serializer.data, status=status.HTTP_200_OK)
+    
+class SearchCustom(APIView):
+    permission_classes = (AllowAny,)
+    def get(self,request):
+        user = User.objects.all()
+        if request.data:
+            data = request.data
+        else:
+            return Response(UserSerializer(user, many=True).data, status=status.HTTP_200_OK)
+        if  'ageL' in data:
+            today = timezone.now().date()
+            age_lower = date(year=today.year - request.data['ageL'], month=today.month, day=today.day)
+            user = user.filter(birthday__lt = age_lower)
+        if 'ageU' in data:
+            today = timezone.now().date()
+            age_upper = date(year=today.year - request.data['ageU'], month=today.month, day=today.day)
+            user = user.filter(birthday__gt = age_upper)
+        if 'first_name' in data:
+            user = user.filter(first_name=data['first_name'])
+        if 'last_name' in data:
+            user = user.filter(last_name=data['last_name'])
+        if 'categories' in data:
+            user = user.filter(favorites__contains=data['categories'])
+        return Response(UserSerializer(user, many=True).data, status=status.HTTP_200_OK)
